4123c0437b01617d67c302d0d1f67c26395880b3
a-l-l-e-g-r-a <barneaw@pedarecc.sa.edu.au>
2026-07-21 03:32:26 +0000

    T3W1 Starter

--- starter.py
+++ starter.py
@@ -2,3 +2,12 @@
 raw_phrase = "aP!pL3e#S4aU%cE"
 
 # YOUR GOAL: Clean up this phrase using the 3 Security Rules below!
+symbols_to_remove = "!#%"
+clean_phrase = " "
+
+for char in raw_phrase:
+    if char not in symbols_to_remove and not char.isdigit():
+        clean_phrase += char
+
+final_code = clean_phrase.upper()
+print(f'Decoded code: {final_code}')
