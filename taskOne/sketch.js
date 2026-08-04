e722913da000d85c337a1666af0e17559e100a6e
a-l-l-e-g-r-a <barneaw@pedarecc.sa.edu.au>
2026-08-04 03:34:07 +0000

    W3 -> Fixed data loading and Created warning signs

--- taskOne/sketch.js
+++ taskOne/sketch.js
@@ -55,19 +55,21 @@ function draw() {
   // 2. Render Dashboard Graphics
   if (aquariumData) { // data from API listed here:
     let temp = aquariumData[0].exps.temperature.curr; //if this value is > 23, warning is displayed
-    let ph = aquariumData[0].exps.ph.curr;
+    let ph = aquariumData[0].exps.ph.curr; //if this value is > 8, warning is displayed
     let nh3 = aquariumData[0].exps.nh3.curr;
     let nh4 = aquariumData[0].exps.nh4.curr;
     let o2 = aquariumData[0].exps.o2.curr;
     let lux = aquariumData[0].exps.lux.curr;
+    let historyData = aquariumData[0].exps.HistoryData;
 
     // Call your custom graphic widgets
     drawTempWidget(50, 120, temp);
-    drawGaugeWidget(300, 120, "pH Level", ph, 6.0, 8.5);
+    drawPHWidget(300, 120, "pH Level", ph, 6.0, 8.5);
     drawGaugeWidget(550, 120, "Ammonia (NH3)", nh3, 0.0, 0.05);
     drawGaugeWidget(550, 280, "Nitrate (NH4)", nh4, 0.0, 1.0);
     drawGaugeWidget(300, 280, "Oxygen (O2)", o2, 0.0, 10.0);
     drawGaugeWidget(50, 280, "Lux", lux, 0, 10000);
+    drawTempHistoryWidget(50, 420, aquariumData[0].exps.HistoryData);
 
   } else {
     // Loading State
@@ -80,7 +82,7 @@ function draw() {
 // TEMPERATURE CARD
 function drawTempWidget(x, y, tempVal) {
   // Background
-  if (tempVal > 23 or tempVal < 15) {
+  if (tempVal >= 23 || tempVal <= 15) {
     fill(255, 0, 0); // Red for wrong temp 
   }
   else {
@@ -90,19 +92,20 @@ function drawTempWidget(x, y, tempVal) {
   rect(x, y, 200, 150, 10);
   // Label
   noStroke();
-  if (tempVal > 23) {
+  if (tempVal >= 23) {
     fill(255);
     textSize(14);
     text("Water Temp", x + 15, y + 15);
     fill(255);
     text("WARNING: HIGH TEMP \nDETECTED", x + 15, y + 40);
   }
-  elif (tempVal < 15) {
+  else if (tempVal <= 15) {
     fill(255);
     textSize(14);
     text("Water Temp", x + 15, y + 15);
     fill(255);
     text("WARNING: LOW TEMP \nDETECTED", x + 15, y + 40);
+  }
   else {
     fill(180, 200, 220);
     textSize(14);
@@ -111,12 +114,40 @@ function drawTempWidget(x, y, tempVal) {
   // Value
   fill(255);
   textSize(36);
-  if (tempVal > 23 or tempVal < 15) {
+  if (tempVal >= 23 || tempVal <= 15) {
     text(tempVal + "°C", x + 15, y + 80);
   } 
   else {
     text(tempVal + "°C", x + 15, y + 50);
   }
+}
+
+// pH individual widget
+function drawPHWidget(x, y, label, val, minVal, maxVal) {
+  if (val >= 8) {
+    fill(255, 0, 0);
+    noStroke();
+    rect(x, y, 200, 150, 10);
+    fill(255);
+    textSize(14);
+    text(label, x + 15, y + 15);
+    fill(255);
+    text("WARNING: HIGH pH \nDETECTED", x + 15, y + 40);
+    fill(255);
+    textSize(36);
+    text(val + " pH", x + 15, y + 80);
+  } 
+  else {
+    fill(0, 71, 100);
+    noStroke();
+    rect(x, y, 200, 150, 10);
+    fill(180, 200, 220);
+    textSize(14);
+    text(label, x + 15, y + 15);
+    fill(255);
+    textSize(36);
+    text(val + " pH", x + 15, y + 50);
+}
 }
 
 // Example Widget Function: Simple Bar Gauge
@@ -134,3 +165,14 @@ function drawGaugeWidget(x, y, label, val, minVal, maxVal) {
   textSize(28);
   text(val, x + 15, y + 50);
 }
+
+function drawTempHistoryWidget(x, y, historyData) {
+  fill(0, 71, 100);
+  stroke(60, 80, 110);
+  rect(x, y, 700, 150, 10);
+
+  noStroke();
+  fill(180, 200, 220);
+  textSize(14);
+  text("Temperature History", x + 15, y + 15);
+
